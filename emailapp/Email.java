e0fcf842e339894627a8407d292c4c1098bdaf95
Prathampthk <prathampthk13@gmail.com>
2025-06-24 12:50:39 -0500

    Initial commit

---
+++ emailapp/Email.java
@@ -0,0 +1,27 @@
+package emailapp;
+
+public class Email {
+    private String lastName;
+    private String firstName;
+    private String password;
+    private String deparment;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and the last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+    }
+
+    // Ask for the deparment
+
+    // Generate a random passoword
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
+}
